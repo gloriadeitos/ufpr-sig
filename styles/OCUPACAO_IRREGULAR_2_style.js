2f3ac5000e89c36090cc2a4296632b465fdfd1b2
Gloria <alvugy@gmail.com>
2024-12-14 14:34:00 -0300

    Legendas arrumadas

--- styles/OCUPACAO_IRREGULAR_2_style.js
+++ styles/OCUPACAO_IRREGULAR_2_style.js
@@ -20,7 +20,7 @@ var style_OCUPACAO_IRREGULAR_2 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(114,155,111,1.0)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(247, 205, 250,1.0)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
